4a946cf8b0c713761d6e8b0dae389ad8a7f7ba1a
jose-donato <43375532+jose-donato@users.noreply.github.com>
2021-01-24 18:54:13 +0000

    Update content.py

--- scripts/lib/content.py
+++ scripts/lib/content.py
@@ -1,14 +1,14 @@
 endpoints = {
     "next_sless": {
-        "url": "http://2.81.115.234:3001/api/dominantColor",
-        "echo": "http://2.81.115.234:3001/api/echo"
+        "url": "http://SERVER_IP_RUNNING_APPLICATIONS:3001/api/dominantColor",
+        "echo": "http://SERVER_IP_RUNNING_APPLICATIONS:3001/api/echo"
     },
     "netlify": {
-        "url": "http://2.81.115.234:8888/.netlify/functions/dominantColor",
-        "echo": "http://2.81.115.234:8888/.netlify/functions/echo"
+        "url": "http://SERVER_IP_RUNNING_APPLICATIONS:8888/.netlify/functions/dominantColor",
+        "echo": "http://SERVER_IP_RUNNING_APPLICATIONS:8888/.netlify/functions/echo"
     },
     "express": {
-        "url": "http://2.81.115.234:3003/api/dominantColor",
-        "echo": "http://2.81.115.234:3003/echo"
+        "url": "http://SERVER_IP_RUNNING_APPLICATIONS:3003/api/dominantColor",
+        "echo": "http://SERVER_IP_RUNNING_APPLICATIONS:3003/echo"
     }
 }
